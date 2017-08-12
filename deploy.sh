d6c7bcba2ea5019ca98b635da803fdddabfcfa0c
kjartab <kjartanbjorset@gmail.com>
2017-08-12 22:06:43 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker-compose down
-docker-compose pull
-docker-compose up -d
+docker-compose -f docker-compose.common.yml -f docker-compose.yml -f docker-compose.prod.yml pull api
+docker-compose -f docker-compose.common.yml -f docker-compose.yml -f docker-compose.prod.yml stop api
+docker-compose -f docker-compose.common.yml -f docker-compose.yml -f docker-compose.prod.yml up -d api
